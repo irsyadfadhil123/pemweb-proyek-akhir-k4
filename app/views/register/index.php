b137ad4278706ae2b3758abd9cdcd2611185a8f7
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-05-08 05:41:05 +0700

    index.php

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -1,9 +1,14 @@
-<form action="<?= BASEURL; ?>/register/tambah" method="post">
-    <label for="nama">Username:</label><br>
-    <input type="text" name="username" id="username" required><br>
-    <label for="">Nama:</label><br>
-    <input type="text" name="nama" id="nama" required><br>
-    <label for="">Password:</label><br>
-    <input type="password" name="password" id="password" required><br>
-    <input type="submit" name="submit" value="register">
-</form>
+        <div class="box1">
+            <div class="box2">
+                <form action="">
+                    <div class="signup-box">
+                        <p>Signup</p>
+                        <input type="text" name="username" placeholder="Username">
+                        <input type="text" name="nama" placeholder="Nama">
+                        <input type="password" name="password" placeholder="Password">
+                        <a href="">I already have an account</a>
+                        <button type="submit">Signup</button>
+                    </div>
+                </form>
+            </div>
+        </div>
