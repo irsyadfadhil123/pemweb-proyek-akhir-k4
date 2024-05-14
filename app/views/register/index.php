41fe24d162bac043b99977eea29fd4f91974a8d1
Irsyad Fadhil <71073281+irsyadfadhil123@users.noreply.github.com>
2024-05-15 05:07:37 +0700

    Update view register

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -1,13 +1,9 @@
         <div class="register-flex-main">
             <form action="<?= BASEURL; ?>/register/tambah" method="post" class="register-flex-box">
                 <p class="register-register-text">Register</p>
-                <input type="text" name="username" placeholder="Username" class="register-text-input">
-                <input type="text" name="nama" placeholder="Nama" class="register-text-input">
-                <input type="password" name="password" placeholder="Password" class="register-text-input">
-                <div class="register-remember">
-                    <input type="checkbox" name="remember">
-                    <label for="remember">Ingat saya</label>
-                </div>
+                <input type="text" name="username" placeholder="Username" class="register-text-input" required>
+                <input type="text" name="nama" placeholder="Nama" class="register-text-input" required>
+                <input type="password" name="password" placeholder="Password" class="register-text-input" required>
                 <a href="<?= BASEURL; ?>/login" class="register-account-link">I already have an account</a>
                 <button type="submit" class="register-submit-button">Register</button>
             </form>
