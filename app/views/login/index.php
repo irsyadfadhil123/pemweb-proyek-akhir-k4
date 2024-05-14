9ec315a52a3ca66f98e5c8d71cf63db8ff1c0a3b
iOSatya <atthoriqsatya@outlook.com>
2024-05-14 21:19:33 +0700

    Update

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,5 +1,5 @@
         <div class="login-flex-main">
-            <form action="" method="post" class="login-flex-box">
+            <form action="<?= BASEURL; ?>/login/verifikasi" method="post" class="login-flex-box">
                 <p class="login-login-text">Login</p>
                 <input type="text" name="username" placeholder="Username" class="login-text-input">
                 <input type="password" name="password" placeholder="Password" class="login-text-input">
@@ -7,7 +7,7 @@
                     <input type="checkbox" name="remember">
                     <label for="remember">Ingat saya</label>
                 </div>
-                <a href="" class="login-account-link">I don't have an account</a>
+                <a href="<?= BASEURL; ?>/register" class="login-account-link">I don't have an account</a>
                 <button type="submit" class="login-submit-button">Login</button>
             </form>
         </div>
--- app/views/register/index.php
+++ app/views/register/index.php
@@ -1,5 +1,5 @@
         <div class="register-flex-main">
-            <form action="" method="post" class="register-flex-box">
+            <form action="<?= BASEURL; ?>/register/tambah" method="post" class="register-flex-box">
                 <p class="register-register-text">Register</p>
                 <input type="text" name="username" placeholder="Username" class="register-text-input">
                 <input type="text" name="nama" placeholder="Nama" class="register-text-input">
@@ -8,7 +8,7 @@
                     <input type="checkbox" name="remember">
                     <label for="remember">Ingat saya</label>
                 </div>
-                <a href="" class="register-account-link">I already have an account</a>
+                <a href="<?= BASEURL; ?>/login" class="register-account-link">I already have an account</a>
                 <button type="submit" class="register-submit-button">Register</button>
             </form>
         </div>
