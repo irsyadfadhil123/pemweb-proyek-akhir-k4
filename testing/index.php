208ca460a8dd367e13ee06c337b0fe042c404b27
iOSatya <atthoriqsatya@outlook.com>
2024-05-20 12:34:26 +0700

    Styling: Edit Profile

--- testing/index.php
+++ testing/index.php
@@ -10,22 +10,27 @@
     </head>
     <body>
         <!--  -->
+        
         <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-            <div class="d-flex flex-column align-items-center border border-secondary rounded p-5 shadow" style="width: 50vw;">
-                <a href="" class="btn btn-outline-warning align-self-start">Kembali</a>
-                <br class="m-3">
-                <img src="..." class="rounded-circle" style="width: 200px; height: 200px;">
-                <br class="m-3">
-                <h2 class="display-6">[INSERT NAME HERE]</h2>
-                <br class="m-3">
-                <div style="width: 400px;">
-                    <label class="lead">Username: </label>
-                    <input type="text" class="form-control" placeholder="[INSERT USERNAME HERE]" disabled>
+            <form action="#" method="post" class="d-flex flex-column border border-secondary rounded p-5 shadow" style="width: 50vw;">
+                <a href="#" class="btn btn-outline-warning align-self-start mb-3">Kembali</a>
+                <input type="hidden" name="password" value="">
+                <div class="input-group mb-3">
+                    <span class="input-group-text">@</span>
+                    <input type="text"name="username" value="" placeholder="Username" required class="form-control">
                 </div>
-                <br class="m-4">
-                <a href="" class="btn btn-success">Edit Profile</a>
-            </div>
+                <input type="text" name="nama" value="" placeholder="Nama" required class="form-control mb-3">
+                <div class="input-group mb-3">
+                    <input type="password" name="newPassword" id="" placeholder="Password Baru" class="form-control">
+                    <input type="password" name="confirmPasword" placeholder="Konfirmasi Password" class="form-control">
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" name="oldPassword" id="" placeholder="Password Lama" required class="form-control">
+                    <button type="submit" class="btn btn-outline-secondary">Submit</button>
+                </div>
+            </form>
         </div>
+
         <!--  -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
