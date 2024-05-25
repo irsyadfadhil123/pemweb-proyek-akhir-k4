4e1bc7a39a94cd981d621d6a1afc44e6a4efe885
iOSatya <atthoriqsatya@outlook.com>
2024-05-25 12:17:10 +0700

    Styling: Buat Tugas

--- app/views/tugas/buat.php
+++ app/views/tugas/buat.php
@@ -1,10 +1,14 @@
-<a href="<?= BASEURL?>/tugas/index">Kembali</a><br>
-
-<form action="<?= BASEURL; ?>/tugas/buat" method="post">
-    <input type="hidden" name="id" value="$_SESSION['id']">
-    <input type="text" name="judul" placeholder="Judul">
-    <input type="text" name="deskripsi" placeholder="Deskripsi">
-    <input type="date" name="deadline" placeholder="Deadline">
-    <input type="text" name="kode_tugas" placeholder="Kode Tugas">
-    <button type="submit">Buat Tugas</button>
-</form>
+        <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+            <form action="<?= BASEURL; ?>/tugas/buat" method="post" class="d-flex flex-column p-5 border border-secondary-subtle rounded align-items-center shadow" style="width: 50vw;">
+                <a href="<?= BASEURL?>/tugas/index" class="btn btn-outline-warning align-self-start">Kembali</a>
+                <h3 class="display-5 mb-3">Buat Tugas</h3>
+                <input type="hidden" name="id" value="$_SESSION['id']">
+                <input type="text" name="judul" placeholder="Judul" class="form-control align-self-center mb-3">
+                <input type="text" name="deskripsi" placeholder="Deskripsi" class="form-control align-self-center mb-3">
+                <input type="text" name="kode_tugas" placeholder="Kode Tugas" class="form-control align-self-center mb-3">
+                <h3 class="lead align-self-start mb-3">Deadline</h3>
+                <input type="date" name="deadline" placeholder="Deadline" class="form-control align-self-center mb-3 align-self-start">
+                
+                <button type="submit" class="btn btn-success align-self-end">Buat Tugas</button>
+            </form>
+        </div>
--- testing/index.php
+++ testing/index.php
@@ -9,17 +9,21 @@
 
     </head>
     <body>
-        <!-- Login, Register, Profile, Edit Profile, Tambah Tugas -->
+        <!-- Login, Register, Profile, Edit Profile, Tambah, Buat -->
         
         
         <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
             <form action="" method="post" class="d-flex flex-column p-5 border border-secondary-subtle rounded align-items-center shadow" style="width: 50vw;">
                 <a href="" class="btn btn-outline-warning align-self-start">Kembali</a>
-                <h3 class="display-5">Tambah Tugas</h3>
-                <br class="mb-4">
-                <input type="text" name="kode_tugas" placeholder="Kode Tugas" required class="form-control align-self-center">
-                <br class="mb-5">
-                <button type="submit" class="btn btn-success align-self-end">Tambah Tugas</button>
+                <h3 class="display-5 mb-3">Buat Tugas</h3>
+                <input type="hidden" name="id" value="$_SESSION['id']">
+                <input type="text" name="judul" placeholder="Judul" class="form-control align-self-center mb-3">
+                <input type="text" name="deskripsi" placeholder="Deskripsi" class="form-control align-self-center mb-3">
+                <input type="text" name="kode_tugas" placeholder="Kode Tugas" class="form-control align-self-center mb-3">
+                <h3 class="lead align-self-start mb-3">Deadline</h3>
+                <input type="date" name="deadline" placeholder="Deadline" class="form-control align-self-center mb-3 align-self-start">
+                
+                <button type="submit" class="btn btn-success align-self-end">Buat Tugas</button>
             </form>
         </div>
 
