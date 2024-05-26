c497793b5690774d9e1a0cfd2670559d5692da4d
iOSatya <atthoriqsatya@outlook.com>
2024-05-26 07:20:36 +0700

    Styling: Upload

--- app/views/tugas/upload.php
+++ app/views/tugas/upload.php
@@ -1,10 +1,49 @@
-<a href="<?= BASEURL?>/tugas/index">Kembali</a>
-<h1><?= $data['tugas']['tugas_id'] ?></h1>
-<a href="<?= BASEURL?>/tugas/kehadiran/<?= $data['tugas']['tugas_id'];?>">Kehadiran</a>
+        <div class="p-4" style="height: 100vh;">
+            <div class="d-flex flex-column p-4 border" style="height: 100%;">
+
+                <div class="d-flex mb-3 justify-content-between">
+                    <a href="<?= BASEURL?>/tugas/index" class="btn btn-outline-warning">Kembali</a>
+                    <div class="align-self-center">
+                        <label class="lead fw-semibold">Nama User</label>
+                    </div>
+                    
+                </div>
+                
+
+                <div class="d-flex mb-3">
+                    <div class="d-flex flex-column flex-grow-1 bg-dark-subtle rounded-start p-3">
+                        <label class="lead"><?= $data['tugas']['tugas_id'] ?></label>
+                        <label class="display-6 fw-medium"><?= $data['tugas']['judul'] ?></label>
+                        <p class="lead"><?= $data['tugas']['deskripsi'] ?></p>
+                    </div>
+                    <div class="d-flex flex-column border border-dark-subtle rounded-end p-3">
+                        <label class="lead">Kode Kelas</label>
+                        <hr>
+                        <label class="display-6 text-secondary">D081</label>
+                    </div>
+                </div>
+
+                <div class="d-flex align-self-end mb-3">
+                    <a href="<?= BASEURL?>/tugas/kehadiran/<?= $data['tugas']['tugas_id'];?>" class="btn btn-info me-3">Catat Kehadiran</a>
+                    <a href="" class="btn btn-info">Upload File</a>
+                </div>
 
 
-<h2><?= $data['tugas']['judul'] ?></h2>
-<h2><?= $data['tugas']['deskripsi'] ?></h2>
-<h2><?= $data['tugas']['deadline'] ?></h2>
+                <div class="d-flex flex-column bg-dark-subtle rounded align-self-start p-2 mb-3">
+                    <label class="lead fw-medium">Deadline</label>
+                    <div class="border-bottom border-secondary lead"><?= $data['tugas']['deadline'] ?></div>
+                </div>
 
-<h1>Forum Diskusi:</h1>
+                <div class="d-flex">
+                    <h1 class="display-6">Forum Diskusi</h1>
+                </div>
+
+                <div class="d-flex flex-column bg-dark-subtle rounded p-3 mb-3" style="height: 100%;">
+                    <label class="lead fw-semibold">Nama User</label>
+                    <p class="lead">Isi Diskusi</p>
+                </div>
+
+                <a href="" class="btn btn-info align-self-end">Buat Diskusi</a>
+
+            </div>
+        </div>
--- testing/index.php
+++ testing/index.php
@@ -9,23 +9,59 @@
 
     </head>
     <body>
-        <!-- Login, Register, Profile, Edit Profile, Tambah, Buat -->
+        <!-- Login, Register, Profile, Edit Profile, Tambah, Buat, Upload -->
         
-        
-        <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-            <form action="" method="post" class="d-flex flex-column p-5 border border-secondary-subtle rounded align-items-center shadow" style="width: 50vw;">
-                <a href="" class="btn btn-outline-warning align-self-start">Kembali</a>
-                <h3 class="display-5 mb-3">Buat Tugas</h3>
-                <input type="hidden" name="id" value="$_SESSION['id']">
-                <input type="text" name="judul" placeholder="Judul" class="form-control align-self-center mb-3">
-                <input type="text" name="deskripsi" placeholder="Deskripsi" class="form-control align-self-center mb-3">
-                <input type="text" name="kode_tugas" placeholder="Kode Tugas" class="form-control align-self-center mb-3">
-                <h3 class="lead align-self-start mb-3">Deadline</h3>
-                <input type="date" name="deadline" placeholder="Deadline" class="form-control align-self-center mb-3 align-self-start">
+        <div class="p-4" style="height: 100vh;">
+            <div class="d-flex flex-column p-4 border" style="height: 100%;">
+
+                <div class="d-flex mb-3 justify-content-between">
+                    <a href="" class="btn btn-outline-warning">Kembali</a>
+                    <div class="align-self-center">
+                        <label class="lead fw-semibold">Nama User</label>
+                    </div>
+                    
+                </div>
                 
-                <button type="submit" class="btn btn-success align-self-end">Buat Tugas</button>
-            </form>
+
+                <div class="d-flex mb-3">
+                    <div class="d-flex flex-column flex-grow-1 bg-dark-subtle rounded-start p-3">
+                        <label class="lead">ID Tugas</label>
+                        <label class="display-6 fw-medium">Judul Tugas</label>
+                        <p class="lead">Deskripsi Tugas</p>
+                    </div>
+                    <div class="d-flex flex-column border border-dark-subtle rounded-end p-3">
+                        <label class="lead">Kode Kelas</label>
+                        <hr>
+                        <label class="display-6 text-secondary">D081</label>
+                    </div>
+                </div>
+
+                <div class="d-flex align-self-end mb-3">
+                    <a href="" class="btn btn-info me-3">Catat Kehadiran</a>
+                    <a href="" class="btn btn-info">Upload File</a>
+                </div>
+
+
+                <div class="d-flex flex-column bg-dark-subtle rounded align-self-start p-2 mb-3">
+                    <label class="lead fw-medium">Deadline</label>
+                    <div class="border-bottom border-secondary lead"></div>
+                </div>
+
+                <div class="d-flex">
+                    <h1 class="display-6">Forum Diskusi</h1>
+                </div>
+
+                <div class="d-flex flex-column bg-dark-subtle rounded p-3 mb-3" style="height: 100%;">
+                    <label class="lead fw-semibold">Nama User</label>
+                    <p class="lead">Isi Diskusi</p>
+                </div>
+
+                <a href="" class="btn btn-info align-self-end">Buat Diskusi</a>
+
+            </div>
         </div>
+
+
 
         <!--  -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
