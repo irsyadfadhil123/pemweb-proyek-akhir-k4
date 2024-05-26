294c9aa6301d6c37c3d51d146a89d24cfe064b7f
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-05-26 13:16:55 +0700

    Styling: Index (#21)
    * Styling: Upload
    
    * Update upload.php
    
    * Update
    
    ---------
    
    Co-authored-by: Irsyad Fadhil <71073281+irsyadfadhil123@users.noreply.github.com>

--- app/views/tugas/index.php
+++ app/views/tugas/index.php
@@ -1,40 +1,69 @@
-<a href="<?= BASEURL;?>/home/index">Kembali</a><br>
-<a href="<?= BASEURL;?>/tugas/tambahTugas">Tambah Tugas</a>
-<a href="<?= BASEURL;?>/tugas/buatTugas">Buat Tugas</a>
+        <div class="d-flex justify-content-between bg-light align-self-stretch p-3 shadow mb-3">
+            <a href="<?= BASEURL;?>/home/index" class="btn btn-outline-warning">Kembali</a>
+            <h3 class="display-6 fs-4 fw-semibold align-self-center">Aplikasi Pengumpul Tugas</h3>
+            <div class="align-self-center">
+                <label class="lead fw-medium">Nama User</label>
+            </div>
+        </div>
 
+        <div class="d-flex flex-column align-items-start p-4">
+            
+            <div class="d-flex mb-3">
+                <a href="<?= BASEURL;?>/tugas/tambahTugas" class="btn btn-outline-success me-3">Tambah Tugas</a>
+                <a href="<?= BASEURL;?>/tugas/buatTugas" class="btn btn-outline-info">Buat Tugas</a>
+            </div>
 
-<h3>Tugas yang tergabung</h3>
-<?php 
-if (!empty($data['tugas'])) {
-    foreach ($data['tugas'] as $tugas_tergabung) {
-        if ($tugas_tergabung['admin'] !== $_COOKIE['id']) {
-?>
-<p><?= $tugas_tergabung['judul']; ?></p>
-<p><?= $tugas_tergabung['deskripsi']; ?></p>
-<p><?= $tugas_tergabung['deadline']; ?></p>
-<a href="<?= BASEURL;?>/tugas/upload/<?= $tugas_tergabung['tugas_id'];?>">upload</a>
-<?php 
-        }
-    }
-} else {
-    echo "Data tugas tidak tersedia.";
-}
-?>
+            <h3 class="display-6 fw-semibold mb-3">Tugas yang ditambahkan</h3>
 
-<h3>Tugas yang dibuat</h3>
-<?php 
-if (!empty($data['tugas'])) {
-    foreach ($data['tugas'] as $tugas_dibuat) {
-        if ($tugas_dibuat['admin'] == $_COOKIE['id']) {
-?>
-<p><?= $tugas_dibuat['judul']; ?></p>
-<p><?= $tugas_dibuat['deskripsi']; ?></p>
-<p><?= $tugas_dibuat['deadline']; ?></p>
-<a href="<?= BASEURL;?>/tugas/lihat/<?= $tugas_dibuat['tugas_id'];?>">lihat</a>
-<?php 
-        }
-    }
-} else {
-    echo "Data tugas tidak tersedia.";
-}
-?>
+            <div class="d-flex mb-3">
+            <?php 
+            if (!empty($data['tugas'])) {
+                foreach ($data['tugas'] as $tugas_tergabung) {
+                    if ($tugas_tergabung['admin'] !== $_COOKIE['id']) {
+            ?>
+                <div class="d-flex flex-column me-4" style="width: 250px; height: 250px;">
+                    <div class="d-flex flex-column bg-secondary p-3 flex-fill text-light">
+                        <label class="lead fw-semibold"><?= $tugas_tergabung['judul']; ?></label>
+                        <label class="lead fs-6"><?= $tugas_tergabung['deskripsi']; ?></label>
+                    </div>
+                    <div class="d-flex bg-warning p-3 pt-2 pb-2 justify-content-between">
+                        <label class="align-self-center"><?= $tugas_tergabung['deadline']; ?></label>
+                        <a href="<?= BASEURL;?>/tugas/upload/<?= $tugas_tergabung['tugas_id'];?>" class="btn btn-secondary">Upload</a>
+                    </div>
+                </div>
+            <?php 
+                    }
+                }
+            } else {
+                echo "Data tugas tidak tersedia.";
+            }
+            ?>
+            </div>
+
+            <h3 class="display-6 fw-semibold mb-3">Tugas yang dibuat</h3>
+
+            <div class="d-flex mb-3">
+            <?php 
+            if (!empty($data['tugas'])) {
+                foreach ($data['tugas'] as $tugas_dibuat) {
+                    if ($tugas_dibuat['admin'] == $_COOKIE['id']) {
+            ?>
+                <div class="d-flex flex-column me-4" style="width: 250px; height: 250px;">
+                    <div class="d-flex flex-column bg-secondary p-3 flex-fill text-light">
+                        <label class="lead fw-semibold"><?= $tugas_dibuat['judul']; ?></label>
+                        <label class="lead fs-6"><?= $tugas_dibuat['deskripsi']; ?></label>
+                    </div>
+                    <div class="d-flex bg-warning p-3 pt-2 pb-2 justify-content-between">
+                        <label class="align-self-center"><?= $tugas_dibuat['deadline']; ?></label>
+                        <a href="<?= BASEURL;?>/tugas/lihat/<?= $tugas_dibuat['tugas_id'];?>" class="btn btn-secondary">Lihat</a>
+                    </div>
+                </div>
+            <?php 
+                    }
+                }
+            } else {
+                echo "Data tugas tidak tersedia.";
+            }
+            ?>
+            </div>
+        </div>
