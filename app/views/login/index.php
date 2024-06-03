43cae4b94350f2da10074f5e5232d69612fe9272
iOSatya <atthoriqsatya@outlook.com>
2024-06-03 10:34:32 +0700

    Redesign

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -3,28 +3,32 @@
                 <div class="d-flex justify-content-center align-items-center bg-dark flex-fill" style="height: 100vh;">
                     <Transition name="slide-fade">
                         <form v-if="show" action="<?= BASEURL; ?>/login/verifikasi" method="post" class="d-flex flex-column border border-secondary-subtle p-4 shadow">
-                            <p class="display-5 text-light">Login</p>
+                            <p class="text-light fs-1" style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">Login</p>
                             <input type="text" name="username" placeholder="Username" class="form-control mb-2 shadow" required>
                             <input type="password" name="password" placeholder="Password" class="form-control mb-2 shadow" required>
                             <div class="form-check mb-2">
                                 <input type="checkbox" name="remember" class="form-check-input">
                                 <label for="remember" class="form-check-label text-light">Ingat saya</label>
                             </div>
-                            <a href="<?= BASEURL; ?>/register" class="align-self-end link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-4">I don't have an account</a>
-                            <button type="submit" class="btn btn-outline-warning align-self-start shadow">Login</button>
+                            <a href="<?= BASEURL; ?>/register" class="align-self-end link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-2">I don't have an account</a>
+                            <?php Flasher::flash();?>
+                            <button type="submit" class="btn btn-outline-warning align-self-start shadow mt-2">Login</button>
+
                         </form>
                     </Transition>
                 </div>
-                <div class="flex-fill">
-                    
+                <div class="flex-fill d-flex align-items-center justify-content-center">
+                    <Transition name="slide-fade-quotes">
+                        <figure v-if="show" class="text-end">
+                            <blockquote class="blockquote">
+                                <p>Educating the mind without educating the heart is no education at all.</p>
+                            </blockquote>
+                            <figcaption class="blockquote-footer">
+                                <label>Aristotle</label>
+                            </figcaption>
+                        </figure>
+                    </Transition>
                 </div>
-                <!-- flasher -->
-                <div class="row">
-                    <div class="col-lg-6">
-                        <?php Flasher::flash();?>
-                    </div>
-                </div>
-                <!-- flasher -->
             </div>
         </div>
 
@@ -53,7 +57,21 @@
 
             .slide-fade-enter-from,
             .slide-fade-leave-to {
-              transform: translateX(20px);
+              transform: translateX(30px);
+              opacity: 0;
+            }
+
+            .slide-fade-quotes-enter-active {
+              transition: all 0.3s ease-out;
+            }
+
+            .slide-fade-quotes-leave-active {
+              transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+            }
+
+            .slide-fade-quotes-enter-from,
+            .slide-fade-quotes-leave-to {
+              transform: translateX(-30px);
               opacity: 0;
             }
         </style>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,9 +4,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $data['judul'];?></title>
-        <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        
     </head>
     <body>
