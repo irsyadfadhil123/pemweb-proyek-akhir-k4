37c9a817aef9befc97305868d1406c8f6970362e
iLimerencc <22081010120@student.upnjatim.ac.id>
2024-06-04 14:11:08 +0700

    task iLimerencc

---
+++ app/views/tugas/update1.php
@@ -0,0 +1,10 @@
+<a href="<?= BASEURL?>/tugas/index">Kembali</a>
+<h1><?= $data['tugas']['tugas_id'] ?></h1>
+<a href="<?= BASEURL?>/tugas/updateTugas/<?= $data['tugas']['tugas_id'] ?>">Update Tugas</a>
+
+<h2><?= $data['tugas']['judul'] ?></h2>
+<h2><?= $data['tugas']['deskripsi'] ?></h2>
+<h2><?= $data['tugas']['deadline'] ?></h2>
+
+
+<h1>Forum Diskusi:</h1>
