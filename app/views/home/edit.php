bc52714204326d5dd42894ae9a8df53806d52af5
iOSatya <atthoriqsatya@outlook.com>
2024-06-20 16:20:04 +0700

    Update edit.php

--- app/views/home/edit.php
+++ app/views/home/edit.php
@@ -16,7 +16,7 @@
 
           <div class="input-group mb-2 mt-2">
             <label class="form-control bg-dark text-light" style="cursor: pointer; width: 30%;" for="file-upload">Browse File</label>
-            <input  type="file" style="display: none;" name="gambar" id="file-upload" onchange="displayFileName()">
+            <input  type="file" style="display: none;" onchange="displayFileName()" name="gambar" id="file-upload">
             <span class="form-control bg-dark text-secondary" style="width: 70%;" id="file-name">File Name</span>
           </div>
 
@@ -42,6 +42,7 @@
               <input class="form-control bg-dark text-light" style="width: 70%;" type="password" name="confirmPassword">
             </div>
           </div>
+          
           <div class="input-group">
             <span class="input-group-text bg-dark text-light">Password Lama</span>
             <input class="form-control form-control bg-dark text-light" type="password" name="oldPassword" required>
@@ -76,99 +77,3 @@
     document.getElementById('file-name').innerText = fileName;
   }
 </script>
-
-
-
-
-
-<!-- ---------------------------------------------------------------------------------------------------- -->
-
-<div class="header">
-    <a href="<?= BASEURL;?>/home/profil" class="btn btn-outline-warning">Kembali</a>
-    <h3 class="display-6 fs-4 fw-semibold">Edit Profile</h3>
-    <label class="lead fw-medium">Nama User</label>
-</div>
-
-<div class="content">
-    <form action="<?= BASEURL;?>/home/editProfil" method="post" enctype="multipart/form-data" class="form-container">
-        <h3 class="display-5 mb-3">Edit Profile</h3>
-        <!-- fitur update gambar -->
-        <input type="file" name="gambar" class="form-control">
-        <!-- fitur update gambar -->                
-        <input type="hidden" name="password" value="<?= $data['profil']['password'] ?>">
-        <div class="form-group">
-            <input type="text" name="username" value="<?= $data['profil']['username'] ?>" placeholder="Username" required class="form-control">
-        </div>
-        <div class="form-group">
-            <input type="text" name="nama" value="<?= $data['profil']['nama'] ?>" placeholder="Nama" required class="form-control">
-        </div>
-        <div class="form-group">
-            <input type="password" name="newPassword" placeholder="Password Baru" class="form-control">
-        </div>
-        <div class="form-group">
-            <input type="password" name="confirmPassword" placeholder="Konfirmasi Password" class="form-control">
-        </div>
-        <div class="input-group">
-            <input type="password" name="oldPassword" placeholder="Password Lama" required class="form-control">
-            <div class="input-group-append">
-                <button type="submit" class="btn btn-outline-secondary">Submit</button>
-            </div>
-        </div>
-    </form>
-</div>
-
-<!-- flasher -->
-<div class="row">
-    <div class="col-lg-6">
-        <?php Flasher::flash();?>
-    </div>
-</div>
-<!-- flasher --> 
-
-<style>
-    body {
-        background-color: #2c2c2c;
-        color: white;
-        }
-    .header {
-        background-color: #343a40;
-        padding: 10px 20px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    .header h3, .header label {
-        color: white;
-    }
-    .content {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 85vh;
-        }
-        .form-container {
-        background-color: #343a40;
-        padding: 20px;
-        border-radius: 5px;
-        width: 75vw;
-    }
-    .form-container h3 {
-        color: white;
-        text-align: center;
-        margin-bottom: 20px;
-    }
-    .form-container input, .form-container button {
-        margin-bottom: 15px;
-    }
-    .form-container input[type="file"] {
-        border: none;
-    }
-    .btn-outline-warning, .btn-outline-secondary {
-        border-color: #ffc107;
-        color: #ffc107;
-    }
-    .btn-outline-warning:hover, .btn-outline-secondary:hover {
-        background-color: #ffc107;
-        color: black;
-    }
-</style>
